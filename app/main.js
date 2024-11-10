53a26c4e1691c4e287b626bf7871bb4be563392b
Achraf (abz) <abz@odoo.com>
2024-11-10 04:23:28 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -22,25 +22,50 @@ const TOKENS = {
   ")": "RIGHT_PAREN",
   "{": "LEFT_BRACE",
   "}": "RIGHT_BRACE",
+
   ",": "COMMA",
   ".": "DOT",
   "-": "MINUS",
   "+": "PLUS",
   ";": "SEMICOLON",
   "*": "STAR",
+
+  "=" : {
+    "default": "EQUAL",
+    "=": "EQUAL_EQUAL"
+  },
+
+  // "==": "EQUAL_EQUAL",
 }
 
 
 const scan_chars = fileContent => {
   const lines = fileContent.split("\n");
   let inError = false;
-  lines.forEach((line, line_index) => {
-    [...line].forEach((char, char_index) => {
-      const desc = TOKENS[char] || null;
-      if (desc)
-        console.log(`${desc} ${char} null`);
-      else {
-        console.error(`[line ${line_index + 1}] Error: Unexpected character: ${char}`)
+  lines.forEach((line, lineIndex) => {
+    let ignoredIndexes = [];
+    [...line].forEach((char, charIndex) => {
+      if (ignoredIndexes.includes(charIndex)) {
+        return ;
+      }
+      const tokenData = TOKENS[char] || null;
+      let tokenName = null;
+      let token = null;
+      if (tokenData) {
+        if (typeof tokenData === "string" || charIndex == line.length - 1) {
+          tokenName = typeof tokenData === "string" ? tokenData : tokenData.default;
+          token = char;
+        }  else {
+          const nextChar = line[charIndex + 1];
+          tokenName = tokenData[nextChar] || tokenData.default;
+          token = nextChar in tokenData ? `${char}${nextChar}`: char;
+          if (nextChar in tokenData) {
+            ignoredIndexes.push(charIndex + 1);
+          }
+        }   
+        console.log(`${tokenName} ${token} null`);
+      } else {
+        console.error(`[line ${lineIndex + 1}] Error: Unexpected character: ${char}`)
         inError = true;
       }
     })
