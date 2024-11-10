04f18d6ddd5faded25bbda6fb61d12f52054ab29
Achraf (abz) <abz@odoo.com>
2024-11-10 03:45:31 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -22,6 +22,12 @@ const TOKENS = {
   ")": "RIGHT_PAREN",
   "{": "LEFT_BRACE",
   "}": "RIGHT_BRACE",
+  ",": "COMMA",
+  ".": "DOT",
+  "-": "MINUS",
+  "+": "PLUS",
+  ";": "SEMICOLON",
+  "*": "STAR",
 }
 
 
