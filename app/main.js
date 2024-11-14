129a77643d218feddcdd26e3054da3d9c78fda42
Achraf (abz) <abz@odoo.com>
2024-11-14 05:43:23 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -2,53 +2,49 @@ import fs from "fs";
 import { printToken, tokenize } from "./tokenizer.js";
 import { printAst } from "./parser.js";
 
-const args = process.argv.slice(2);
-
-if (args.length < 2) {
-  console.error("Usage: ./your_program.sh tokenize <filename>");
-  process.exit(1);
-}
-
-// const command = args[0];
-
 const Commands = {
   "tokenize": ([fileName]) => {
-    // const filename = args[1];
-    const fileContent = fs.readFileSync(filename, "utf8");
+    const fileContent = fs.readFileSync(fileName, "utf8");
     const {returnCode, tokens} = tokenize(fileContent);
-    if (returnCode) {
-      console.log("TokenizerError. Can't print tokens");
-      return returnCode;
-    }
+    // if (returnCode) {
+    //   console.log("TokenizerError. Can't print tokens");
+    //   return returnCode;
+    // }
     printToken(tokens);
     return returnCode;
   },
   "parse": ([fileName]) => {
-    const fileContent = fs.readFileSync(filename, "utf8");
+    const fileContent = fs.readFileSync(fileName, "utf8");
     const { returnCode: tokenizerReturnCode, tokens} = tokenize(fileContent);
     if (tokenizerReturnCode) {
       console.log("TokenizerError. Can't parse ast.");
       return tokenizerReturnCode;
     }
-    const {returnCode, ast} = parse(tokens);
-    if (returnCode) {
-      console.log("ParserError. Can't print tokens");
-
-      return returnCode;
-    }
-    return printAst(ast);
+    // const {returnCode, ast} = parse(tokens);
+    // if (returnCode) {
+    //   console.log("ParserError. Can't print tokens");
+      
+    //   return returnCode;
+    // }
+    // return printAst(ast);
+    return printAst(tokens);
   }
 }
 
-const command = Commands[args[0]];
-if (command) {
-  console.log(args);
-  const returnCode = command(args.slice(1));
-  process.exit(returnCode);
-} else {
-  console.error(`Usage: Unknown command: ${command}.`);
-  console.error(`List of available commands: ${Object.keys(Commands).join(",")}.`)
+const [commandName, ...args] = process.argv.slice(2);
+
+if (!args.length) {
+  console.error("Usage: ./your_program.sh tokenize <filename>");
   process.exit(1);
+} else {
+  if (commandName in Commands) {
+    const returnCode = Commands[commandName](args);
+    process.exit(returnCode);
+  } else {
+    console.error(`Usage: Unknown command: ${commandName}.`);
+    console.error(`List of available commands: ${Object.keys(Commands).join(",")}.`)
+    process.exit(1);
+  }
 }
 
 
--- app/parser.js
+++ app/parser.js
@@ -1,6 +1,77 @@
-const printAst = (fileContent) => {
+class Expression {
+    
+    accept(){
+
+    }
+}
+
+class Visitor extends Expression {
+
+    parenthesize(){
+
+    }
 
 }
+
+
+class AstPrinter extends Visitor {
+    print(expression) {
+        return expression.accept(this);
+    }
+    visitLiteral(expression){
+        return expression.value === null ? "nil" : expression.value.toString();
+    }
+}
+class Literal extends Expression {
+    constructor(value) {
+        super();
+        this.value = value;
+    }
+
+    accept(visitor) {
+        return visitor.visitLiteral(this);
+    }
+
+}
+
+class Binary extends Expression {
+    constructor(leftExpression, operator, rightExpression) {
+        this.left = leftExpression;
+        this.operator = operator;
+        this.right = rightExpression;
+    }
+}
+
+class Grouping extends Expression {
+    constructor(expression){
+        this.expression = expression;
+    }
+    
+}
+
+class Unary extends Expression {
+    constructor(operator, rightExpression){
+        this.operator = operator;
+        this.right = rightExpression;
+    }
+}
+
+
+
+const printAst = (tokens) => {
+    const printer = new AstPrinter();
+    tokens.forEach(token => {
+        if (["TRUE", "FALSE", "NIL"].includes(token.tokenType)){
+            const expression = new Literal(token.lexeme);
+            const printed = printer.print(expression);
+            console.log(printed);
+        }
+    })
+}
+
+
+
+
 
 export {
     printAst,
